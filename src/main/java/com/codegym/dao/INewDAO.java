3c4a06d5f686943bbe4119282d35924c17d2dd61
nguyentrongdoan <nguyendoan206@gmail.com>
2020-08-22 09:56:42 +0700

    them newmodel

---
+++ src/main/java/com/codegym/dao/INewDAO.java
@@ -0,0 +1,5 @@
+package com.codegym.dao;
+
+public interface INewDAO {
+	
+}
---
+++ src/main/java/com/codegym/dao/impl/NewDAO.java
@@ -0,0 +1,7 @@
+package com.codegym.dao.impl;
+
+import com.codegym.dao.INewDAO;
+
+public class NewDAO implements INewDAO{
+	
+}
